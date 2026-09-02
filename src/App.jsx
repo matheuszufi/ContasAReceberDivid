cd44fdc8c92744460a57048d14b3f2a7777cd907
Matheus Zufi <88894519+matheuszufi@users.noreply.github.com>
2026-09-02 10:29:28 -0300

    save
    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
-import Login from './pages/Login'
-import Dashboard from './pages/Dashboard'
-import Imoveis from './pages/Imoveis'
-import ImoveisTodos from './pages/ImoveisTodos'
-import CadastrarImovel from './pages/CadastrarImovel'
-import Proprietarios from './pages/Proprietarios'
-import CadastrarProprietario from './pages/CadastrarProprietario'
-import Inquilinos from './pages/Inquilinos'
-import CadastrarInquilino from './pages/CadastrarInquilino'
-import ImportarPlanilha from './pages/ImportarPlanilha'
-import Inadimplentes from './pages/Inadimplentes'
-import CadastrarInadimplencia from './pages/CadastrarInadimplencia'
-import CadastrarConta from './pages/CadastrarConta'
-import CadastrarSeguro from './pages/CadastrarSeguro'
-import ImportarInadimplencia from './pages/ImportarInadimplencia'
-import TimelineInadimplencia from './pages/TimelineInadimplencia'
-import SeguroFianca from './pages/SeguroFianca'
-import SeguroIncendio from './pages/SeguroIncendio'
-import Desocupacoes from './pages/Desocupacoes'
-import Perfil from './pages/Perfil'
 import { useAuth } from './auth'
 import { firebaseError } from './firebase'
+
+// Cada página é carregada sob demanda (code-splitting por rota), para que o CSS de
+// cada uma fique isolado no seu próprio chunk e não seja baixado/aplicado nas outras páginas
+const Login                  = lazy(() => import('./pages/Login'))
+const Dashboard               = lazy(() => import('./pages/Dashboard'))
+const Imoveis                 = lazy(() => import('./pages/Imoveis'))
+const ImoveisTodos            = lazy(() => import('./pages/ImoveisTodos'))
+const CadastrarImovel         = lazy(() => import('./pages/CadastrarImovel'))
+const Proprietarios           = lazy(() => import('./pages/Proprietarios'))
+const CadastrarProprietario   = lazy(() => import('./pages/CadastrarProprietario'))
+const Inquilinos              = lazy(() => import('./pages/Inquilinos'))
+const CadastrarInquilino      = lazy(() => import('./pages/CadastrarInquilino'))
+const ImportarPlanilha        = lazy(() => import('./pages/ImportarPlanilha'))
+const Inadimplentes           = lazy(() => import('./pages/Inadimplentes'))
+const CadastrarInadimplencia  = lazy(() => import('./pages/CadastrarInadimplencia'))
+const CadastrarConta          = lazy(() => import('./pages/CadastrarConta'))
+const CadastrarSeguro         = lazy(() => import('./pages/CadastrarSeguro'))
+const ImportarInadimplencia   = lazy(() => import('./pages/ImportarInadimplencia'))
+const TimelineInadimplencia   = lazy(() => import('./pages/TimelineInadimplencia'))
+const SeguroFianca            = lazy(() => import('./pages/SeguroFianca'))
+const SeguroIncendio          = lazy(() => import('./pages/SeguroIncendio'))
+const Desocupacoes            = lazy(() => import('./pages/Desocupacoes'))
+const Perfil                  = lazy(() => import('./pages/Perfil'))
+
 
 function PrivateRoute({ children }) {
   const { user, loading } = useAuth()
@@ -67,32 +71,35 @@ export default function App() {
   }
 
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/dashboard"                  element={<PrivateRoute><Dashboard /></PrivateRoute>} />
-      <Route path="/imoveis"                    element={<PrivateRoute><Imoveis /></PrivateRoute>} />
-      <Route path="/imoveis-todos"              element={<PrivateRoute><ImoveisTodos /></PrivateRoute>} />
-      <Route path="/imoveis/cadastrar"          element={<PrivateRoute><CadastrarImovel /></PrivateRoute>} />
-      <Route path="/imoveis/editar/:id"         element={<PrivateRoute><CadastrarImovel /></PrivateRoute>} />
-      <Route path="/proprietarios"              element={<PrivateRoute><Proprietarios /></PrivateRoute>} />
-      <Route path="/proprietarios/cadastrar"    element={<PrivateRoute><CadastrarProprietario /></PrivateRoute>} />
-      <Route path="/proprietarios/editar/:id"   element={<PrivateRoute><CadastrarProprietario /></PrivateRoute>} />
-      <Route path="/inquilinos"                 element={<PrivateRoute><Inquilinos /></PrivateRoute>} />
-      <Route path="/inquilinos/cadastrar"       element={<PrivateRoute><CadastrarInquilino /></PrivateRoute>} />
-      <Route path="/inquilinos/editar/:id"      element={<PrivateRoute><CadastrarInquilino /></PrivateRoute>} />
-      <Route path="/inquilinos/importar"        element={<PrivateRoute><ImportarPlanilha /></PrivateRoute>} />
-      <Route path="/inadimplentes"              element={<PrivateRoute><Inadimplentes /></PrivateRoute>} />
-      <Route path="/inadimplentes/importar"     element={<PrivateRoute><ImportarInadimplencia /></PrivateRoute>} />
-      <Route path="/inadimplentes/cadastrar"    element={<PrivateRoute><CadastrarInadimplencia /></PrivateRoute>} />
-      <Route path="/inadimplentes/editar/:id"   element={<PrivateRoute><CadastrarInadimplencia /></PrivateRoute>} />
-      <Route path="/inadimplentes/timeline/:id" element={<PrivateRoute><TimelineInadimplencia /></PrivateRoute>} />
-      <Route path="/contas/cadastrar"           element={<PrivateRoute><CadastrarConta /></PrivateRoute>} />
-      <Route path="/seguros/cadastrar"          element={<PrivateRoute><CadastrarSeguro /></PrivateRoute>} />
-      <Route path="/seguro-fianca"               element={<PrivateRoute><SeguroFianca /></PrivateRoute>} />
-      <Route path="/seguro-incendio"             element={<PrivateRoute><SeguroIncendio /></PrivateRoute>} />
-      <Route path="/desocupacoes"                element={<PrivateRoute><Desocupacoes /></PrivateRoute>} />
-      <Route path="/perfil"                      element={<PrivateRoute><Perfil /></PrivateRoute>} />
-    </Routes>
+    <Suspense fallback={<div className="loading-screen">Carregando...</div>}>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard"                  element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+        <Route path="/imoveis"                    element={<PrivateRoute><Imoveis /></PrivateRoute>} />
+        <Route path="/imoveis-todos"              element={<PrivateRoute><ImoveisTodos /></PrivateRoute>} />
+        <Route path="/imoveis/cadastrar"          element={<PrivateRoute><CadastrarImovel /></PrivateRoute>} />
+        <Route path="/imoveis/editar/:id"         element={<PrivateRoute><CadastrarImovel /></PrivateRoute>} />
+        <Route path="/proprietarios"              element={<PrivateRoute><Proprietarios /></PrivateRoute>} />
+        <Route path="/proprietarios/cadastrar"    element={<PrivateRoute><CadastrarProprietario /></PrivateRoute>} />
+        <Route path="/proprietarios/editar/:id"   element={<PrivateRoute><CadastrarProprietario /></PrivateRoute>} />
+        <Route path="/inquilinos"                 element={<PrivateRoute><Inquilinos /></PrivateRoute>} />
+        <Route path="/inquilinos/cadastrar"       element={<PrivateRoute><CadastrarInquilino /></PrivateRoute>} />
+        <Route path="/inquilinos/editar/:id"      element={<PrivateRoute><CadastrarInquilino /></PrivateRoute>} />
+        <Route path="/inquilinos/importar"        element={<PrivateRoute><ImportarPlanilha /></PrivateRoute>} />
+        <Route path="/inadimplentes"              element={<PrivateRoute><Inadimplentes /></PrivateRoute>} />
+        <Route path="/inadimplentes/importar"     element={<PrivateRoute><ImportarInadimplencia /></PrivateRoute>} />
+        <Route path="/inadimplentes/cadastrar"    element={<PrivateRoute><CadastrarInadimplencia /></PrivateRoute>} />
+        <Route path="/inadimplentes/editar/:id"   element={<PrivateRoute><CadastrarInadimplencia /></PrivateRoute>} />
+        <Route path="/inadimplentes/timeline/:id" element={<PrivateRoute><TimelineInadimplencia /></PrivateRoute>} />
+        <Route path="/contas/cadastrar"           element={<PrivateRoute><CadastrarConta /></PrivateRoute>} />
+        <Route path="/seguros/cadastrar"          element={<PrivateRoute><CadastrarSeguro /></PrivateRoute>} />
+        <Route path="/seguro-fianca"               element={<PrivateRoute><SeguroFianca /></PrivateRoute>} />
+        <Route path="/seguro-incendio"             element={<PrivateRoute><SeguroIncendio /></PrivateRoute>} />
+        <Route path="/desocupacoes"                element={<PrivateRoute><Desocupacoes /></PrivateRoute>} />
+        <Route path="/perfil"                      element={<PrivateRoute><Perfil /></PrivateRoute>} />
+      </Routes>
+    </Suspense>
   )
 }
+
 
